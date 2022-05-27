1040e4ee97dd7654259c78d6ad73af5dcfb6da17
Michael Cheung <michaelcheung.cheme@gmail.com>
2022-05-27 15:34:22 -0600

    styling for squares

---
+++ client/src/Components/MemoryGame.js
@@ -0,0 +1,22 @@
+import '../StyleSheets/MemGame.css'
+
+const MemoryGame = () =>{
+
+  
+
+  return(
+
+    <div>
+      <h1>Memory Game</h1>
+      <div className="grid">
+      <div className="green"></div>
+      <div className="blue"></div>
+      <div className="red"></div>
+      <div className="yellow"></div>
+      </div>
+    </div>
+  )
+
+}
+
+export default MemoryGame
