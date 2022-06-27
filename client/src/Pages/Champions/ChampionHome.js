76500b39a6bb64655e914c12598e9550799c1e64
Michael Cheung <michaelcheung.cheme@gmail.com>
2022-06-27 01:32:04 -0600

    code prep done

--- client/src/Pages/Champions/ChampionHome.js
+++ client/src/Pages/Champions/ChampionHome.js
@@ -1,37 +1,60 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const ChampionHome = () => {
+  const [champs, setChamps] = useState([]);
+  const [norm, setNorm] = useState([])
 
+  useEffect(() => {
+    getChamp();
+  }, []);
 
-const ChampionHome =()=>{
-  
-  const [champs, setChamps] = useState([])
-  
-  useEffect(()=>{
-    getChamp()
-  },[])
+  const getChamp = async () => {
+    let res = await axios.get("api/allChamp");
+    let res2 = await axios.get("api/locations");
+    setChamps(res.data);
+    setNorm(normChamp(res.data, res2.data));
+  };
 
-  const getChamp = async()=>{
-    let res = await axios.get('api/allChamp')
-    setChamps(res.data)
-  }
+  const normChamp = (data, locData) => {
+    const ids = data.map((c) => c.location_id);
+    const uniqueId = [...new Set(ids)];
+    return uniqueId.map((location) => {
+      let champs = data.filter((c) => c.location_id === location);
+      let name = locData.filter((l) => l.id == location)[0].name;
+      return {name,champs}
+    });
+  };
 
   const renderChamps = () => {
-    return champs.map(({name,id})=>{
+    return champs.map(({ name, id }) => {
       return (
         <div key={id}>
           <h1>{name}</h1>
         </div>
-      )
-    })
+      );
+    });
+  };
+
+  const renderNormChamps = () =>{
+    return norm.map((n)=>(
+      <div>
+        <h1>{n.name}</h1>
+        {n.champs.map((c)=>(
+          <div>
+            <h3>{c.name}</h3>
+          </div>
+        ))}
+      </div>
+    ))
   }
 
-  return(
+  return (
     <div>
       <h1>Champion Home</h1>
-    {renderChamps()}
+      {renderNormChamps()}
     </div>
-  )
-}
+  );
+};
 
-export default ChampionHome
+export default ChampionHome;
